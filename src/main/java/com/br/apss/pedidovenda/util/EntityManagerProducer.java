3322d9ebff52a919ded93d0a8b0cb2255a53e6f5
paraguai <paraguai@apss>
2017-11-17 23:07:11 -0300

    Configuração do Heroku

--- src/main/java/com/br/apss/pedidovenda/util/EntityManagerProducer.java
+++ src/main/java/com/br/apss/pedidovenda/util/EntityManagerProducer.java
@@ -1,9 +1,11 @@
 package com.br.apss.pedidovenda.util;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -12,21 +14,24 @@ import org.hibernate.Session;
 
 @ApplicationScoped
 public class EntityManagerProducer {
-	
-	private EntityManagerFactory factory;
-	
-	public EntityManagerProducer() {
-		this.factory = Persistence.createEntityManagerFactory("PedidoPU");
-	}
-	
-	@Produces
-	@RequestScoped
-	public Session  createEntityManager() {
+
+    @Inject
+    private PersistenceProperties properties;
+    
+    private EntityManagerFactory factory;
+
+    @PostConstruct
+    public void postConstruct() {
+        this.factory = Persistence.createEntityManagerFactory("PedidoPU", properties.get());
+    }
+
+    @Produces
+    @RequestScoped
+    public Session  createEntityManager() {
 		return (Session) this.factory.createEntityManager();
 	}
-	
-	public void closeEntityManager(@Disposes EntityManager manager) {
-		manager.close();
-	}
 
+    public void closeEntityManager(@Disposes EntityManager manager) {
+        manager.close();
+    }
 }
